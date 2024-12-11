c926a7f03e4a5febefa90c7b9db91a98ab9d6e21
vrishaanpatel@gmail.com <vrishaanpatel@gmail.com>
2024-12-11 13:40:54 +0000

    Created Form1; Edited Login

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import LoginTemplate
+Adminfrom ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
 import anvil.users
@@ -8,32 +8,11 @@ from anvil.google.drive import app_files
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..Home import Home
 
-class Login(LoginTemplate):
+
+class Form1(Form1Template):
   def __init__(self, **properties):
-    
+    # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    while not anvil.users.login_with_form():
-      pass
-     
-    user= anvil.users.get_user()
-    if user['admin'] is False:
-      open_form('Main')
-    else:
-      open_form('Admin')
-      
-
-  
-  
-    
-
-
-  
-  
-        
-
-    
-    
 
     # Any code you write here will run when the form opens.
