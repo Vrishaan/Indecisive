8a6157b6241744ec5e6b6bf9240d0ce5d097ac16
vrishaanpatel@gmail.com <vrishaanpatel@gmail.com>
2025-01-08 07:02:16 +0000

    Edited Shop

--- client_code/Shop/__init__.py
+++ client_code/Shop/__init__.py
@@ -9,7 +9,6 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from ..Product import Product
-
 class Shop(ShopTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -17,10 +16,42 @@ class Shop(ShopTemplate):
     
     self.banner.role = ['spaced-title', 'left-right-padding']
     
-    products = app_tables.products.search()
-    for p in products:
-      self.flow_panel_1.add_component(Product(item=p), width='30%')
+    # Load all products from the database
+    self.all_products = list(app_tables.products.search())
+    
+    # Initially display all products
+    self.display_products(self.all_products)
 
-  def flow_panel_1_show(self, **event_args):
-    """This method is called when the FlowPanel is shown on the screen"""
+  def display_products(self, products):
+    """Populate the FlowPanel with products"""
+    self.flow_panel_1.clear()  # Clear the FlowPanel
+    
+    if products:
+      # Add each product as a component to the FlowPanel
+      for p in products:
+        self.flow_panel_1.add_component(Product(item=p), width='30%')
+    else:
+      # Display a message if no products match the search query
+      self.flow_panel_1.add_component(
+          Label(text="No products found.", align="center", role="headline")
+      )
+
+  def search_box_pressed_enter(self, **event_args):
+    """Called when the user presses Enter in the search box"""
+    search_query = self.search_box.text.lower()  # Get the search query in lowercase
+    
+    # Filter products by name or description
+    filtered_products = [
+      p for p in self.all_products
+      if search_query in p['name'].lower() or search_query in p['description'].lower()
+    ]
+    
+    # Display the filtered products or a no-match message
+    self.display_products(filtered_products)
+    
+    # Clear the search box after taking input
+    self.search_box.text = ""
+
+  def shop_button_click(self, **event_args):
+    self.display_products(self.all_products)
     pass
