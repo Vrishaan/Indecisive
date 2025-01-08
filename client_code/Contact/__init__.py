ea439663c7d42628a07ada98dd6a7ec89d146102
vrishaanpatel@gmail.com <vrishaanpatel@gmail.com>
2025-01-08 09:23:32 +0000

    Edited settings and Contact

--- client_code/Contact/__init__.py
+++ client_code/Contact/__init__.py
@@ -1,50 +1,65 @@
 from ._anvil_designer import ContactTemplate
 from anvil import *
 import anvil.users
-import stripe.checkout
 import anvil.server
 import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from datetime import datetime
 
 class Contact(ContactTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
+    
+    # Initialize the map
     self.map_1.center = GoogleMap.LatLng(19.00493908512311, 72.82994174280753)
     self.map_1.zoom = 15
     icon = GoogleMap.Icon(url="_/theme/maps-icon.png")
     self.marker = GoogleMap.Marker(animation=GoogleMap.Animation.DROP, 
-                              position=GoogleMap.LatLng(19.00493908512311, 72.82994174280753),
-                              icon = icon)
+                                    position=GoogleMap.LatLng(19.00493908512311, 72.82994174280753),
+                                    icon=icon)
     self.map_1.add_component(self.marker)
-    
+
     def marker_click(sender, **event_args):
-      i =GoogleMap.InfoWindow(content=Label(text="Indecisive Clothing Store", 
-                                            bold=True, 
-                                            foreground="theme:Primary 700"))
+      i = GoogleMap.InfoWindow(content=Label(text="Indecisive Clothing Store", 
+                                             bold=True, 
+                                             foreground="theme:Primary 700"))
       i.open(self.map_1, sender)
 
     self.marker.set_event_handler("click", marker_click)
 
+    # Populate the dropdown with product names and "Other"
+    products = app_tables.products.search()
+    product_names = [p['name'] for p in products] + ["Other"]
+    self.product_dropdown.items = product_names
+    self.product_dropdown.selected_value = None  # Default to no selection
+
   def send_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    name = self.name_box.text
-    email = self.email_box.text
-    message = self.message_box.text
-    
-    if name and email and message:
-      anvil.server.call('add_message', name, email, message)
-      self.name_box.text = ""
-      self.email_box.text = ""
-      self.message_box.text = ""
-      alert("Thanks for getting in touch!")
-    else:
-      alert("Please fill out the entire form before sending your message")
+    # Get logged-in user information
+    user = anvil.users.get_user()
+    if user:
+      # Access user properties directly
+      email = user['email'] 
       
+      about = self.product_dropdown.selected_value
+      message = self.message_box.text
 
+      if about and message:
+        # Add feedback to the Feedback database
+        app_tables.feedback.add_row(
+          email=email,
+          about=about,
+          message=message,
+          date=datetime.now()
+        )
 
-
-
-
+        # Clear form fields
+        self.product_dropdown.selected_value = None
+        self.message_box.text = ""
+        alert("Thanks for your feedback!")
+      else:
+        alert("Please select a product (or 'Other') and provide your message before sending.")
+    else:
+      alert("You must be logged in to send feedback.")
